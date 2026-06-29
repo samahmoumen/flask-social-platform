0ee88940134662968b02543f9520827306cdc556
samahmoumen <samahmn98@gmail.com>
2026-06-29 23:11:16 +0100

    added login form

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,32 @@
 from app import app
-from flask import render_template
-
+from flask import render_template, flash, redirect
+from forms import LoginForm
 #the two  @app.route lines above the function are decorators, 
 @app.route('/')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
 @app.route('/index')
 def index():
-    user = { 'username' : 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
-    
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
 #A decorator modifies the function that follows it. A common pattern with decorators is to use them to register functions as callbacks for certain events. In this case, the @app.route decorator creates an association between the URL given as an argument and the function. 
